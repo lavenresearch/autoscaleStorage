09b8fe93067b89229050f836ff629dbebffdcfa1
Su Yi <suyi@hust.edu.cn>
2015-04-27 21:39:31 +0800

    update genfile.py to make it write files endlessly

--- genfile.py
+++ genfile.py
@@ -1,11 +1,12 @@
 import os
 
-cmd = "dd if=/dev/zero of=/autoscale/test1g bs=1M count=10"
-count = 100
-
-for i in range(100):
-    os.popen(cmd)
-    os.popen("sync")
-    print "10MB writen"
+cmd1 = "dd if=/dev/zero of=/autoscale/test1g"
+cmd2 = "  bs=1M count=10"
+count = 50
+while True:
+    for i in range(count):
+        os.popen(cmd1+str(i)+cmd2)
+        os.popen("sync")
+        print "10MB writen"
 
 print "1GB writen"
